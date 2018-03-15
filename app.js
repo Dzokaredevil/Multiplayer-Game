56015649bd8ec663367d4aab99b14160638d3960
Dzokaredevil <dima.voytovich@mail.ru>
2018-03-16 01:33:04 +0300

    Version 13

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var mongojs = require("mongojs");
-var db = mongojs('localhost:27017/myGame', ['account','progress']);
+//var mongojs = require("mongojs");
+var db = null;//mongojs('localhost:27017/myGame', ['account','progress']);
 
 var express = require('express');
 var app = express();
@@ -10,7 +10,7 @@ app.get('/',function(req, res) {
 });
 app.use('/client',express.static(__dirname + '/client'));
 
-serv.listen(2000);
+serv.listen(process.env.PORT || 2000);
 console.log("Server started.");
 
 var SOCKET_LIST = {};
@@ -54,6 +54,7 @@ var Player = function(id){
 	var super_update = self.update;
 	self.update = function(){
 		self.updateSpd();
+		
 		super_update();
 		
 		if(self.pressingAttack){
@@ -230,25 +231,28 @@ Bullet.getAllInitPack = function(){
 var DEBUG = true;
 
 var isValidPassword = function(data,cb){
-	db.account.find({username:data.username,password:data.password},function(err,res){
+	return cb(true);
+	/*db.account.find({username:data.username,password:data.password},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
-	});
+	});*/
 }
 var isUsernameTaken = function(data,cb){
-	db.account.find({username:data.username},function(err,res){
+	return cb(false);
+	/*db.account.find({username:data.username},function(err,res){
 		if(res.length > 0)
 			cb(true);
 		else
 			cb(false);
-	});
+	});*/
 }
 var addUser = function(data,cb){
-	db.account.insert({username:data.username,password:data.password},function(err){
+	return cb();
+	/*db.account.insert({username:data.username,password:data.password},function(err){
 		cb();
-	});
+	});*/
 }
 
 var io = require('socket.io')(serv,{});
