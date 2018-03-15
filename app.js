5dcd9afe59abcf81eaf7cbbfb678026742381f44
Dzokaredevil <dima.voytovich@mail.ru>
2018-03-16 01:36:06 +0300

    Version 14

--- app.js
+++ app.js
@@ -15,14 +15,26 @@ console.log("Server started.");
 
 var SOCKET_LIST = {};
 
-var Entity = function(){
+var Entity = function(param){
 	var self = {
 		x:250,
 		y:250,
 		spdX:0,
 		spdY:0,
 		id:"",
+		map:'forest',
 	}
+	if(param){
+		if(param.x)
+			self.x = param.x;
+		if(param.y)
+			self.y = param.y;
+		if(param.map)
+			self.map = param.map;
+		if(param.id)
+			self.id = param.id;		
+	}
+	
 	self.update = function(){
 		self.updatePosition();
 	}
@@ -36,9 +48,8 @@ var Entity = function(){
 	return self;
 }
 
-var Player = function(id){
-	var self = Entity();
-	self.id = id;
+var Player = function(param){
+	var self = Entity(param);
 	self.number = "" + Math.floor(10 * Math.random());
 	self.pressingRight = false;
 	self.pressingLeft = false;
@@ -62,9 +73,13 @@ var Player = function(id){
 		}
 	}
 	self.shootBullet = function(angle){
-		var b = Bullet(self.id,angle);
-		b.x = self.x;
-		b.y = self.y;
+		Bullet({
+			parent:self.id,
+			angle:angle,
+			x:self.x,
+			y:self.y,
+			map:self.map,
+		});
 	}
 	
 	self.updateSpd = function(){
@@ -92,6 +107,7 @@ var Player = function(id){
 			hp:self.hp,
 			hpMax:self.hpMax,
 			score:self.score,
+			map:self.map,
 		};		
 	}
 	self.getUpdatePack = function(){
@@ -104,14 +120,20 @@ var Player = function(id){
 		}	
 	}
 	
-	Player.list[id] = self;
+	Player.list[self.id] = self;
 	
 	initPack.player.push(self.getInitPack());
 	return self;
 }
 Player.list = {};
 Player.onConnect = function(socket){
-	var player = Player(socket.id);
+	var map = 'forest';
+	if(Math.random() < 0.5)
+		map = 'field';
+	var player = Player({
+		id:socket.id,
+		map:map,
+	});
 	socket.on('keyPress',function(data){
 		if(data.inputId === 'left')
 			player.pressingLeft = data.state;
@@ -155,12 +177,14 @@ Player.update = function(){
 }
 
 
-var Bullet = function(parent,angle){
-	var self = Entity();
+var Bullet = function(param){
+	var self = Entity(param);
 	self.id = Math.random();
-	self.spdX = Math.cos(angle/180*Math.PI) * 10;
-	self.spdY = Math.sin(angle/180*Math.PI) * 10;
-	self.parent = parent;
+	self.angle = param.angle;
+	self.spdX = Math.cos(param.angle/180*Math.PI) * 10;
+	self.spdY = Math.sin(param.angle/180*Math.PI) * 10;
+	self.parent = param.parent;
+	
 	self.timer = 0;
 	self.toRemove = false;
 	var super_update = self.update;
@@ -171,7 +195,7 @@ var Bullet = function(parent,angle){
 		
 		for(var i in Player.list){
 			var p = Player.list[i];
-			if(self.getDistance(p) < 32 && self.parent !== p.id){
+			if(self.map === p.map && self.getDistance(p) < 32 && self.parent !== p.id){
 				p.hp -= 1;
 								
 				if(p.hp <= 0){
@@ -190,7 +214,8 @@ var Bullet = function(parent,angle){
 		return {
 			id:self.id,
 			x:self.x,
-			y:self.y,		
+			y:self.y,
+			map:self.map,
 		};
 	}
 	self.getUpdatePack = function(){
